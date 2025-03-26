c752079ed3763b30ca46aafd3af6862ea4dfcac3
Takae <93442902+takaesa@users.noreply.github.com>
2025-03-26 14:47:16 +0700

    add axios interceptor

--- React-query-demo/react-query-demo/hooks/useSuperHeroesData.js
+++ React-query-demo/react-query-demo/hooks/useSuperHeroesData.js
@@ -1,12 +1,14 @@
 import { useQuery, useMutation, useQueryClient } from "react-query"
-import axios from "axios"
+import { request } from "../utils/axios-utils"
 
 const fetchSuperHeroes = () => {
-    return axios.get('https://localhost:4001/superheroes')
+    // return axios.get('https://localhost:4001/superheroes')
+    return request({ url: '/superheroes' })
 }
 
 const addSuperHero = (hero) => {
-    return axios.post('https://localhost:4001/superheroes', hero)
+    // return axios.post('https://localhost:4001/superheroes', hero)
+    return request({ url: '/superheroes', method: 'post', data: hero })
 }
 const useSuperHeroesData = (onSuccess, onError) => {
     return useQuery(
@@ -27,15 +29,36 @@ const useSuperHeroesData = (onSuccess, onError) => {
 export const useAddSuperHeroData = () => {
     const queryClient = useQueryClient()
     return useMutation(addSuperHero, {
-        onSuccess: (data) => {
-            // queryClient.invalidateQueries('super-heroes')
+        // onSuccess: (data) => {
+        //     // queryClient.invalidateQueries('super-heroes')
+        // queryClient.setQueryData('super-heroes', (oldQueryData) => {
+        //     return {
+        //         ...oldQueryData,
+        //         data: [...oldQueryData.data, data.data]
+
+        //     }
+        // })
+        // }
+        onMutate: async (newHero) => {
+            await queryClient.cancelQueries('super-heroes')
+
+            const previousHeroData = queryClient.getQueryData('super-heroes')
+
             queryClient.setQueryData('super-heroes', (oldQueryData) => {
                 return {
                     ...oldQueryData,
-                    data: [...oldQueryData.data, data.data]
+                    data: [...oldQueryData.data, { id: oldQueryData?.data?.length + 1, ...newHero }]
 
                 }
             })
-        }
+
+            return { previousHeroData }
+        },
+        onError: (_error, _hero, context) => {
+            queryClient.setQueryData('super-heroes', context.previousHeroData)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries('super-heroes')
+        },
     })
 }
