1792853b98a73a02723c7a447d77142ad5ab6064
Takae <93442902+takaesa@users.noreply.github.com>
2025-03-27 13:50:46 +0700

    update catch all route basic example

---
+++ study-nextjs-app/pages/docs/[[...params]].js
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+
+
+function Dot() {
+    const router = useRouter();
+    const { params = [] } = router.query;
+    console.log(params)
+
+    if (params.length === 2) {
+        return
+        (
+            <h1>Viewing docs for feature {params[0]}, and concept {params[1]}</h1 >
+        )
+    }
+    else if (params / length === 1) {
+        return
+        (
+            <h1>Viewing docs for feature {params[0]}</h1>
+        )
+    }
+
+    return (
+        <h1>Doc home Page</h1>
+    )
+}
+
+export default Dot;
---
+++ study-nextjs-app/pages/product/[productId]/review/[reviewId].js
@@ -0,0 +1,16 @@
+const { useRouter } = require("next/router")
+
+
+function Review() {
+    const router = useRouter();
+    const { productId, reviewId } = router.query;
+
+    return (
+        <h1>
+            Review {reviewId} for product {productId}
+        </h1>
+    )
+
+}
+
+export default Review;
